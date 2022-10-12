44e1be1cfc7f8f837caaf3d821a05ba73007344d
Rafael Librero <librerorafael@outlook.com>
2022-10-12 19:02:34 +0200

    NavigationView en DrawerLayout

--- app/src/main/java/com/rafaellibrero/programacionmultimedia/DrawerNavigation.java
+++ app/src/main/java/com/rafaellibrero/programacionmultimedia/DrawerNavigation.java
@@ -1,14 +1,25 @@
 package com.rafaellibrero.programacionmultimedia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class DrawerNavigation extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer_navigation);
+
+        drawerLayout = findViewById(R.id.navigator);
+        navigationView = findViewById(R.id.nav_view);
     }
 }
