038b056107906f72fd65e9a45ebd9c2e6bed4bd9
Rafael Librero <librerorafael@outlook.com>
2022-10-12 20:06:11 +0200

    Segunda actividad dentro de DrawerNavigation

--- app/src/main/java/com/rafaellibrero/programacionmultimedia/DrawerNavigation.java
+++ app/src/main/java/com/rafaellibrero/programacionmultimedia/DrawerNavigation.java
@@ -1,14 +1,19 @@
 package com.rafaellibrero.programacionmultimedia;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class DrawerNavigation extends AppCompatActivity {
+
+    private EditText name2,mail2,number2;
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
@@ -19,7 +24,33 @@ public class DrawerNavigation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer_navigation);
 
+        name2 = findViewById(R.id.etName2);
+        mail2 = findViewById(R.id.etEmail2);
+        number2 = findViewById(R.id.etPhone2);
         drawerLayout = findViewById(R.id.navigator);
         navigationView = findViewById(R.id.nav_view);
+
+        String valor = getIntent().getStringExtra("nombre");
+        name2.setText(valor);
+        valor = getIntent().getStringExtra("email");
+        mail2.setText(valor);
+        valor = getIntent().getStringExtra("telefono");
+        number2.setText(valor);
+
+        navigationView.bringToFront();
+        /*ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();*/
+    }
+
+    @Override
+    public void onBackPressed(){
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
     }
 }
