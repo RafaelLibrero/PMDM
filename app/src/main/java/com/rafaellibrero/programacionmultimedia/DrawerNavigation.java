24d492dd7620ac07dd49ccd7f1152ba260d4547a
Rafael Librero <librerorafael@outlook.com>
2022-10-30 18:58:42 +0100

    La opcion pintura funciona

--- app/src/main/java/com/rafaellibrero/programacionmultimedia/DrawerNavigation.java
+++ app/src/main/java/com/rafaellibrero/programacionmultimedia/DrawerNavigation.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -54,6 +56,13 @@ public class DrawerNavigation extends AppCompatActivity implements NavigationVie
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.nav_paint:
+                Intent intent = new Intent(DrawerNavigation.this,PictureActivity.class);
+                startActivity(intent);
+                break;
+        }
         return true;
     }
 }
