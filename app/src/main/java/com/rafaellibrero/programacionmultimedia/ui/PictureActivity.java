d57572609abe16440f327c3699ccfa90d24e9c98
Rafael Librero <librerorafael@outlook.com>
2022-10-31 00:43:04 +0100

    Entrega 3 Listados
    Elementos añadidos al recyclerview

--- app/src/main/java/com/rafaellibrero/programacionmultimedia/ui/PictureActivity.java
+++ app/src/main/java/com/rafaellibrero/programacionmultimedia/ui/PictureActivity.java
@@ -5,10 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import com.rafaellibrero.programacionmultimedia.R;
 import com.rafaellibrero.programacionmultimedia.ui.recycler.ListElement;
@@ -29,6 +27,13 @@ public class PictureActivity extends AppCompatActivity {
         elements.add(new ListElement("La creación de Adán", "1512", "Capilla Sixtina", R.drawable.creation_of_adam));
         elements.add(new ListElement("El Juicio Final", "1541", "Capilla Sixtina", R.drawable.last_judgement));
         elements.add(new ListElement("Doni Tonda", "1507", "Galería de los Uffizi", R.drawable.tondo_doni));
+        elements.add(new ListElement("El Diluvio Universal", "1509", "Capilla Sixtina", R.drawable.diluvio_universal));
+        elements.add(new ListElement("La Creación de Eva", "1510", "Capilla Sixtina", R.drawable.creation_of_eva));
+        elements.add(new ListElement("David y Goliat","1509", "Capilla Sixtina", R.drawable.david_and_goliat));
+        elements.add(new ListElement("Sibila Délfica", "1509", "Capilla Sixtina", R.drawable.the_delphic_sibyl));
+        elements.add(new ListElement("Sibila Líbica", "1512", "Capilla Sixtina", R.drawable.sibila_libica));
+        elements.add(new ListElement("Venus y Cupido", "1533", "Galería de la Academia de Florencia", R.drawable.venus_cupido));
+        elements.add(new ListElement("Santo Entierro", "1501", "Galería Nacional de Londres", R.drawable.santo_entierro));
 
         RVAdapter recyclerViewAdapter = new RVAdapter(elements);
 
@@ -40,7 +45,6 @@ public class PictureActivity extends AppCompatActivity {
         recyclerViewAdapter.setListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                CharSequence[] opciones={"Ok"};
                 AlertDialog.Builder datos = new AlertDialog.Builder(PictureActivity.this);
                 datos.setTitle(elements.get(recyclerView.getChildAdapterPosition(view)).getName());
                 datos.setMessage("\nAño de realización: "+elements.get(recyclerView.getChildAdapterPosition(view)).getYear()+
@@ -48,8 +52,6 @@ public class PictureActivity extends AppCompatActivity {
                 datos.show();
             }
         });
-
-
 
     }
 }
