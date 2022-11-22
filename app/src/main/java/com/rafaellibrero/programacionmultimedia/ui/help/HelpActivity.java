d7b4dcc96add28a32e8d75dde1668afc0ac8e643
Rafael Librero <librerorafael@outlook.com>
2022-11-22 20:37:56 +0100

    Botones y flags

--- app/src/main/java/com/rafaellibrero/programacionmultimedia/ui/help/HelpActivity.java
+++ app/src/main/java/com/rafaellibrero/programacionmultimedia/ui/help/HelpActivity.java
@@ -2,19 +2,24 @@ package com.rafaellibrero.programacionmultimedia.ui.help;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.res.AssetManager;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
+import com.rafaellibrero.programacionmultimedia.DrawerNavigation;
+import com.rafaellibrero.programacionmultimedia.MainActivity;
 import com.rafaellibrero.programacionmultimedia.R;
 
 public class HelpActivity extends AppCompatActivity {
 
-    VideoView videoView;
-    MediaController mediaController;
+    private VideoView videoView;
+    private MediaController mediaController;
+    private Button iniciar, volver;
     int position = 0;
 
     @Override
@@ -23,9 +28,12 @@ public class HelpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_help);
 
         videoView = findViewById(R.id.videoview);
+        iniciar = findViewById(R.id.btIniciar);
+        volver = findViewById(R.id.btVolver);
 
         videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.videoplayback));
         videoView.start();
+
 
         this.mediaController = new MediaController(HelpActivity.this);
 
@@ -44,6 +52,24 @@ public class HelpActivity extends AppCompatActivity {
                         mediaController.setAnchorView(videoView);
                     }
                 });
+            }
+        });
+
+        volver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HelpActivity.this, DrawerNavigation.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
+        });
+
+        iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HelpActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
             }
         });
     }
